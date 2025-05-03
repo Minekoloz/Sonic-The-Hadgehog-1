bf88c3628b59c862ed492e91615dbe45017442cd
Minekoloz <minekoloz22@gmail.com>
2025-05-03 17:57:50 +0400

    03.05.2025
    deleted hard things to do

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,27 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public Transform target; 
-    public Vector3 offset = new Vector3(0f, 1f, -10f); 
-    public float smoothSpeed = 0f; 
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 1f, -10f);
 
-    public Vector2 minLimits; 
+    public Vector2 minLimits;
     public Vector2 maxLimits;
+
+    private float verticalOffsetTarget = 0f;
+    private float currentVerticalOffset = 0f;
+    public float smoothSpeed = 5f;
 
     void LateUpdate()
     {
         if (target == null) return;
 
-        Vector3 desiredPosition = target.position + offset;
+        currentVerticalOffset = Mathf.Lerp(currentVerticalOffset, verticalOffsetTarget, smoothSpeed * Time.deltaTime);
+        Vector3 desiredPosition = target.position + new Vector3(offset.x, offset.y + currentVerticalOffset, offset.z);
 
         float clampedX = Mathf.Clamp(desiredPosition.x, minLimits.x, maxLimits.x);
-        float clampedY = Mathf.Clamp(desiredPosition.y, minLimits.y + 10f, maxLimits.y);
+        float clampedY = Mathf.Clamp(desiredPosition.y, minLimits.y, maxLimits.y);
 
         Vector3 clampedPosition = new Vector3(clampedX, clampedY, desiredPosition.z);
-
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, clampedPosition, smoothSpeed * Time.deltaTime);
         transform.position = clampedPosition;
+    }
+
+    public void SetVerticalOffset(float offset)
+    {
+        verticalOffsetTarget = offset;
+    }
+
+    public void ResetVerticalOffset()
+    {
+        verticalOffsetTarget = 0f;
     }
 }
--- Assets/Scripts/SonicController.cs
+++ Assets/Scripts/SonicController.cs
@@ -17,6 +17,13 @@ public class SonicController : MonoBehaviour
 
     private bool isJumping;
     private float currentSpeed;
+    private bool isDucking;
+    private bool isLookingUp;
+
+    public CameraController cameraController;
+
+    public CapsuleCollider2D capsuleCollider;
+    public CircleCollider2D circleCollider;
 
     void Start()
     {
@@ -24,56 +31,108 @@ public class SonicController : MonoBehaviour
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         currentSpeed = moveSpeed;
+
+        cameraController ??= Camera.main.GetComponent<CameraController>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+
+        capsuleCollider.enabled = true;
+        circleCollider.enabled = false;
     }
 
     void Update()
     {
         float move = Input.GetAxis("Horizontal");
+        HandleDuckingAndLookingUp(move);
+        HandleMovement(move);
+        HandleJumping();
+        HandleAnimations(move);
+        FlipSprite(move);
+    }
 
-        if (move != 0)
+    void HandleDuckingAndLookingUp(float move)
+    {
+        bool isDuckingOrLookingUp = (Input.GetKey(KeyCode.S) && isGrounded && Mathf.Abs(move) < 0.1f) || 
+                                      (Input.GetKey(KeyCode.W) && isGrounded && Mathf.Abs(move) < 0.1f);
+
+        if (isDuckingOrLookingUp)
         {
-            currentSpeed += acceleration * Time.deltaTime;
-            currentSpeed = Mathf.Min(currentSpeed, maxSpeed);
+            if (Input.GetKey(KeyCode.S))
+            {
+                isDucking = true;
+                isLookingUp = false;
+                SetAnimation("Sonic_Ducking");
+                cameraController?.SetVerticalOffset(-0.5f);
+                SetColliderState(false, true);
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                isLookingUp = true;
+                isDucking = false;
+                SetAnimation("Sonic_Looking_Up");
+                cameraController?.SetVerticalOffset(0.5f);
+                SetColliderState(true, false);
+            }
+
+            rb.velocity = new Vector2(0f, rb.velocity.y);  // Freeze horizontal movement
         }
         else
         {
-            currentSpeed -= deceleration * Time.deltaTime;
-            currentSpeed = Mathf.Max(currentSpeed, moveSpeed); 
+            ResetDuckingAndLookingUp();
         }
+    }
 
-        rb.velocity = new Vector2(currentSpeed * move, rb.velocity.y);
+    void ResetDuckingAndLookingUp()
+    {
+        isDucking = false;
+        isLookingUp = false;
+        cameraController?.ResetVerticalOffset();
+        SetColliderState(true, false);
+    }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+    void SetColliderState(bool capsuleEnabled, bool circleEnabled)
+    {
+        if (capsuleCollider != null) capsuleCollider.enabled = capsuleEnabled;
+        if (circleCollider != null) circleCollider.enabled = circleEnabled;
+    }
+
+    void HandleMovement(float move)
+    {
+        if (!isDucking && !isLookingUp)
         {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            currentSpeed = Mathf.MoveTowards(currentSpeed, move != 0 ? maxSpeed : moveSpeed, 
+                                             (move != 0 ? acceleration : deceleration) * Time.deltaTime);
+            rb.velocity = new Vector2(currentSpeed * move, rb.velocity.y);
+        }
+    }
+
+    void HandleJumping()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded && !isDucking && !isLookingUp)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
             isJumping = true;
             SetAnimation("Sonic_Spinning");
         }
+    }
 
+    void HandleAnimations(float move)
+    {
         if (isJumping)
         {
             SetAnimation("Sonic_Spinning");
         }
-        else if (isGrounded)
+        else if (isGrounded && !isDucking && !isLookingUp)
         {
-            if (Mathf.Abs(move) > 0.01f)
-            {
-                if (Mathf.Abs(rb.velocity.x) > 8f) 
-                    SetAnimation("Sonic_Run_Fast");
-                else
-                    SetAnimation("Sonic_Running");
-            }
-            else
-            {
-                SetAnimation("Sonic_Idle");
-            }
+            SetAnimation(Mathf.Abs(move) > 0.01f ? (Mathf.Abs(rb.velocity.x) > 8f ? "Sonic_Run_Fast" : "Sonic_Running") : "Sonic_Idle");
         }
+    }
 
-        if (move > 0)
-            spriteRenderer.flipX = false;
-        else if (move < 0)
-            spriteRenderer.flipX = true;
+    void FlipSprite(float move)
+    {
+        if (move > 0) spriteRenderer.flipX = false;
+        else if (move < 0) spriteRenderer.flipX = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -86,13 +145,13 @@ public class SonicController : MonoBehaviour
     }
 
     private void OnCollisionExit2D(Collision2D collision)
-{
-    if (collision.contacts.Length > 0 && collision.contacts[0].normal.y > 0.5f)
     {
-        isGrounded = false;
-        isJumping = true;
+        if (collision.contacts.Length > 0 && collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = false;
+            isJumping = true;
+        }
     }
-}
 
     private void SetAnimation(string animationName)
     {
